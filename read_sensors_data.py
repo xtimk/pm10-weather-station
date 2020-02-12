3918b1252f28d2b825b7640285f80ef3103dcb17
xtimk <onaizitelem@gmail.com>
2020-02-12 13:05:13 +0100

    Implemented readings from dht11 and dht22

--- read_sensors_data.py
+++ read_sensors_data.py
@@ -7,6 +7,10 @@ import time
 import datetime
 import csv
 import os
+import json
+
+from dht22 import get_dht22_temperature_c, get_dht22_humidity
+from dht11 import get_dht11_temperature_c, get_dht11_humidity
 
 def configuration_import(filepath):
 	## Now reading from config file
@@ -24,9 +28,8 @@ def configuration_import(filepath):
 		print("Exiting due to an error.")
 		exit(1)
 
-
-	## Printing config infos
-	print(" * Config YAML Info * ")
+	print(" * DHT11 data pin configured: " + str(config['dht11_data_pin']) )
+	print(" * DHT22 data pin configured: " + str(config['dht22_data_pin']) )
 
 	## If not using basic auth for elasticsearch i will call http(s)://es_address
 	if config['es_basic_auth'] == 0:
@@ -50,6 +53,7 @@ def configuration_import(filepath):
 
 def elasticsearch_connect(es_url):
 	## Try to establish Elasticsearch connection
+	print(" * Creating elasticsearh handle.. *")
 	try:
 		es = Elasticsearch(es_url)
 		return es
@@ -58,12 +62,18 @@ def elasticsearch_connect(es_url):
 		exit(1)
 
 def elasticsearch_createIfNotExistIndex(es_handle, index_name):
+	print(" * Setting elasticsearh index.. *")	
 	## Try to create Elasticsearch index, ignoring 400 caused by IndexAlreadyExistsException
 	try:
 		es_handle.indices.create(index=index_name, ignore=400)
 	except Exception as e:
 		print(e)
 
+def elasticsearch_index_document(es_handle, index_name, document):
+	try:
+		res = es_handle.index(index=index_name, body=document)
+	except Exception as e:
+		print(e)
 
 if __name__ == '__main__':
 	## Getting current dir
@@ -79,3 +89,38 @@ if __name__ == '__main__':
 	config = configuration_import(conf_file)
 	es = elasticsearch_connect(config['es_url'])
 	elasticsearch_createIfNotExistIndex(es, index_name)
+
+	print(" * Reading data from sensors.. * ")
+	timestamp = datetime.datetime.utcnow()
+
+	dht22_temperature = get_dht22_temperature_c(config['dht22_data_pin'])
+	dht22_humidity = get_dht22_humidity(config['dht22_data_pin'])
+
+	dht11_temperature = get_dht11_temperature_c(config['dht11_data_pin'])
+	dht11_humidity = get_dht11_humidity(config['dht11_data_pin'])
+
+	doc = {
+		'dht22_temperature': dht22_temperature,
+		'dht22_humidity': dht22_humidity,
+		'dht11_temperature': dht11_temperature,
+		'dht11_humidity': dht11_humidity,
+		'timestamp': timestamp
+	}
+	print(" * Indexing results into elasticsearch.. * ")
+	elasticsearch_index_document(es, index_name, doc)
+	print(" * All Done * ")
+
+
+	print("\n * ######################## * \n")
+	print("----- SENSORS DATA -----")
+	print(" * DHT11 Sensor data:")
+	print(" * >> Temperature: {0:.2f}°C (±2°C)".format(dht11_temperature))
+	print(" * >> Humidity   : {0:.2f} % (±5%)".format(dht11_humidity))
+	
+	print("------------------------")
+
+	print(" * DHT22 Sensor data:")
+	print(" * >> Temperature: {0:.2f}°C (±0.5°C)".format(dht22_temperature))
+	print(" * >> Humidity   : {0:.2f} % (±2-5 %)".format(dht22_humidity))
+
+	print("------------------------")
