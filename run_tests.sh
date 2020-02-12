eb67854d3671e5dce1fa179d72b489b77f9e4c48
xtimk <onaizitelem@gmail.com>
2020-02-12 12:10:38 +0100

    Added unittests of dht11 and dht22 sensors

---
+++ run_tests.sh
@@ -0,0 +1,2 @@
+echo "Starting tests..."
+python3 -m unittest discover -s tests
---
+++ tests/test_dht22_hw.py
@@ -0,0 +1,39 @@
+import board
+import unittest
+from dht22 import get_dht22_temperature_c, get_dht22_humidity
+
+data_pin = 17
+
+class BasicDHT22Tests(unittest.TestCase):
+
+	## Check if get_dht22_temperature_c returns a float value
+	def test_temperature_celsius_return_type(self):
+		result = get_dht22_temperature_c(data_pin)
+		self.assertIsInstance(result, float)
+
+	## Check if get_dht22_temperature_c returns temperature t.c
+	## -10 <= temperature <= 50
+	def test_temperature_celsius_value_in_human_range(self):
+		temperature = get_dht22_temperature_c(data_pin)
+		result = (temperature >= -10) and (temperature <= 50)
+		self.assertTrue(result)
+
+	## Check if get_dht22_humidity returns a float value
+	def test_humidity_return_type(self):
+		result = get_dht22_humidity(data_pin)
+		self.assertIsInstance(result, float)
+
+	## Check if get_dht22_humidity returns a % value:
+	## 0 <= humidity <= 100
+	def test_humidity_value_in_correct_range(self):
+		humidity = get_dht22_humidity(data_pin)
+		result = (humidity >= 0) and (humidity <= 100)
+		self.assertTrue(result)
+
+if __name__ == '__main__':
+	unittest.main()
+	# print("Temperature: " + str(get_dht22_temperature_c(data_pin)) + "°C")
+	# print("Humidity   : " + str(get_dht22_humidity(data_pin)) + "%")
+
+
+
